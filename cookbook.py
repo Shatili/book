01e780bf501fb39e74533fa6ae225e1300896975
Shatili <Shatili@yandex.ru>
2024-01-22 16:28:47 +0300

    Task number 2

--- cookbook.py
+++ cookbook.py
@@ -13,4 +13,25 @@ with open("recipes.txt", encoding="utf-8") as cook_file:
             })
         cook_book[dish] = ingredients
         cook_file.readline()
-print(cook_book)      
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    list_of_dishes = {}
+    for dish in dishes:
+        for ingredients in cook_book[dish]:
+            ingredients_count = dict([(ingredients['ingredient_name'],
+                                     {'quantity': int(ingredients['quantity']) * person_count,
+                                      'measure': ingredients['measure']})])
+            if list_of_dishes.get(ingredients['ingredient_name']) == 'None':
+                _merger = (int(list_of_dishes[ingredients['ingredient_name']]['quantity']) +
+                           int(ingredients_count[ingredients['ingredient_name']]['quantity']))
+                list_of_dishes[ingredients['ingredient_name']]['quantity'] = _merger
+            else:
+                list_of_dishes.update(ingredients_count)    
+    return list_of_dishes
+print(get_shop_list_by_dishes(['Омлет'], 2))
+            
+            
+        
+            
+
